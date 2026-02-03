bb24c98f282f42f0fc65ec460d61fae72f4d6098
divyanshi2203 <divyanshisaini22@gmail.com>
2026-02-03 23:52:55 +0530

    Why always Sorry?

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def sum_up(l):
     for i in l:
         a = a+i
     return a
+def no_sorry_accepted(i):
+    while (i<11):
+        print("Shut up and I don't want to talk to you")
+        i+=1   
 print(sum_up([3,5,3,7,8,6]))
 print(add(4,7))
+no_sorry_accepted(1)
+print("I hate you")
+
 
     
