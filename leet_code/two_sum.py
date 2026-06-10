137965c4683532ad317107c7efff77dcc53f2734
Divyanshi Saini <divyanshisaini22@gmail.com>
2026-06-10 20:49:30 +0530

    Clarify return statement with a comment
    Added a comment to clarify the return statement.

--- leet_code/two_sum.py
+++ leet_code/two_sum.py
@@ -3,6 +3,6 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
     for i in range(len(nums)):
         other_num = target - nums[i]
         if visited.get(other_num) is not None:
-            return [i,visited.get(other_num)]
+            return [i,visited.get(other_num)] # the final answer
         visited[nums[i]] = i
                  
